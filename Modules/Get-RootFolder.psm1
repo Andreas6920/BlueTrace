5ddeb5704d25d1e9b67caa521441ba21629da327
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-11-19 08:58:39 +0100

    test1

--- Modules/Get-RootFolder.psm1
+++
@@ -1,3 +0,0 @@
-function Get-RootFolder {
-    $Date = Get-Date -Format "yyyy.MM.dd"
-    "Logs_" + $Date + "_" + $Env:COMPUTERNAME + "_" + $Env:USERNAME}
--- Modules/Get-RootPath.psm1
+++
@@ -1,5 +0,0 @@
-function Get-RootPath {
-    $desktop = [Environment]::GetFolderPath("Desktop")
-    if ($desktop -and (Test-Path $desktop)) { return $desktop }
-        if ($env:SystemDrive -and (Test-Path $env:SystemDrive)) {return $env:SystemDrive}
-            if (Test-Path "C:\") {return "C:\"}}
